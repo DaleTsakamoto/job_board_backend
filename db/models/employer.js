3dded69af9d663b9632ae020762b9ca296799c8b
Kyle Barthelmes <kbart2401@gmail.com>
2021-04-14 18:16:46 -0400

    updated sequelize models

--- db/models/employer.js
+++ db/models/employer.js
@@ -2,8 +2,14 @@
 module.exports = (sequelize, DataTypes) => {
   const Employer = sequelize.define('Employer', {
     name: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
     industry: DataTypes.STRING,
-    website: DataTypes.STRING
+    website: DataTypes.STRING,
+    hashedPassword: DataTypes.STRING
   }, {});
   Employer.associate = function (models) {
     Employer.hasMany(models.JobPosting, { foreignKey: 'employerId' })
